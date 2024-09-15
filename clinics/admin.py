d0ac548e798c97e0d4f874158ac6154a5eded14f
Shubham Chawla <shubham_chawla@live.com>
2024-09-14 20:35:51 -0700

    added clinic model

--- clinics/admin.py
+++ clinics/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Clinic
 
 # Register your models here.
+admin.site.register(Clinic)
--- clinics/models.py
+++ clinics/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Clinic(models.Model):
+    name = models.CharField(max_length=120, unique=True, null=False)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=50, null=False)
+    state = models.CharField(max_length=50, null=False)
+    zipcode = models.CharField(max_length=10, null=False, validators=[
+        RegexValidator(
+            regex = r'^(^[0-9]{5}(?:-[0-9]{4})?$|^$)',
+            message = 'Must be valid zipcode in formats 12345 or 12345-1234',
+        )
+    ])
+    email = models.EmailField(unique=True, null=False)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
